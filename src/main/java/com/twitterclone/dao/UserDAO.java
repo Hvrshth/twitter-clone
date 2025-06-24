a37890094b5e8a57458918e4f4b6edbdab0e95b9
Harshith <iharshith512@gmail.com>
2025-06-24 00:08:40 -0400

    Create UserDAO.java

---
+++ src/main/java/com/twitterclone/dao/UserDAO.java
@@ -0,0 +1,33 @@
+package com.twitterclone.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.ServletContext;
+
+import com.twitterclone.util.DBUtil;
+
+public class UserDAO {
+    private final ServletContext context;
+
+    public UserDAO(ServletContext context){
+        this.context = context;
+    }
+
+    // Validates email and password against DB
+    public boolean ValidateUser(String email, String password) throws Exception {
+        String query = "SELECT * FROM USER WHERE email_address = ? AND password = ?";
+        
+        try(Connection conn = DBUtil.getConnection(context);
+        PreparedStatement ps = conn.prepareStatement(query)){
+
+        ps.setString(1, email);
+        ps.setString(2, password);
+        ResultSet rs = ps.executeQuery();
+
+        return rs.next(); // true if user found 
+
+        }
+    }
+}
