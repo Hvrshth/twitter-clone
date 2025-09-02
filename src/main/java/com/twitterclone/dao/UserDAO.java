4e401253eae58b09e290d171bfe3da0344363e45
Harshith <iharshith512@gmail.com>
2025-09-02 15:31:17 -0400

    updated

--- src/main/java/com/twitterclone/dao/UserDAO.java
+++ src/main/java/com/twitterclone/dao/UserDAO.java
@@ -19,7 +19,7 @@ public class UserDAO {
 
     // Validates username and password
     public boolean validateUser(String username, String password) throws Exception {
-        String query = "SELECT * FROM user WHERE username = ? AND password = ?";
+        String query = "SELECT * FROM users WHERE username = ? AND password = ?";
         
         try(Connection conn = DBUtil.getConnection(context);
         PreparedStatement ps = conn.prepareStatement(query)) {
@@ -35,7 +35,7 @@ public class UserDAO {
 
     // Registers new user
     public boolean registerUser(String username, String password) throws Exception {
-        String query = "INSERT INTO user (username, password) VALUES (?, ?)";
+        String query = "INSERT INTO users (username, password) VALUES (?, ?)";
         
         try(Connection conn = DBUtil.getConnection(context);
         PreparedStatement ps = conn.prepareStatement(query)) {
@@ -50,7 +50,7 @@ public class UserDAO {
 
     // Updates password
     public boolean updatePassword(String username, String password) throws Exception {
-        String query = "UPDATE user SET password = ? WHERE username = ?";
+        String query = "UPDATE users SET password = ? WHERE username = ?";
         try(Connection conn = DBUtil.getConnection(context);
         PreparedStatement ps = conn.prepareStatement(query)) {
 
@@ -64,7 +64,7 @@ public class UserDAO {
 
     // Deletes account
     public boolean deleteUser(String username) throws Exception {
-        String query = "DELETE FROM user WHERE username = ?";
+        String query = "DELETE FROM users WHERE username = ?";
         try(Connection conn = DBUtil.getConnection(context);
         PreparedStatement ps = conn.prepareStatement(query)) {
 
